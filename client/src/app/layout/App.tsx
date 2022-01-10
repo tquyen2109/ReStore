30b90e1d36c1aae1c06cb3de4e7143420a9e798c
tquyen2109 <t.quyen1997@yahoo.com>
2022-01-09 20:54:35 -0500

    Add Route

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -3,6 +3,11 @@ import Catalog from "../../features/catalog/Catalog";
 import Header from "./Header";
 import {Container, createTheme, CssBaseline, ThemeProvider} from "@mui/material";
 import {useState} from "react";
+import {Route} from "react-router-dom";
+import HomePage from "../../features/home/HomePage";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import AboutPage from "../../features/about/AboutPage";
+import ContactPage from "../../features/contact/Contact";
 
 function App() {
     const [darkMode, setDarkMode] = useState(false);
@@ -23,7 +28,11 @@ function App() {
         <CssBaseline />
         <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
         <Container>
-            <Catalog />
+            <Route path={'/'} exact component={HomePage}/>
+            <Route path={'/catalog'} exact component={Catalog}/>
+            <Route path={'/catalog/:id'} component={ProductDetails}/>
+            <Route path={'/about'} component={AboutPage}/>
+            <Route path={'/contact'} component={ContactPage}/>
         </Container>
     </ThemeProvider>
   );
