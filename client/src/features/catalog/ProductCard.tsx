34fe3a2a3415eac1db08e8d2459d0fed079a0bc8
tquyen2109 <t.quyen1997@yahoo.com>
2022-01-10 20:43:07 -0500

    Product card

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -7,6 +7,7 @@ import {
     Typography
 } from "@mui/material";
 import {Product} from "../../app/models/product";
+import {Link} from "react-router-dom";
 interface Props {
     product: Product;
 }
@@ -38,7 +39,7 @@ export default function ProductCard ({product}: Props) {
             </CardContent>
             <CardActions>
                 <Button size="small">Share</Button>
-                <Button size="small">View</Button>
+                <Button component={Link} to={`/catalog/${product.id}`} size="small">View</Button>
             </CardActions>
         </Card>
     )
