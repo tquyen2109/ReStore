fb014fad3274e2ffcb9047593c56acb623ffa628
tquyen2109 <t.quyen1997@yahoo.com>
2022-01-26 18:30:13 -0500

    Protected Route and redirecting

--- client/src/features/account/Login.tsx
+++ client/src/features/account/Login.tsx
@@ -10,7 +10,7 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import {Paper} from "@mui/material";
-import {Link, useHistory} from "react-router-dom";
+import {Link, useHistory, useLocation} from "react-router-dom";
 import {FieldValues, useForm} from "react-hook-form";
 import {LoadingButton} from "@mui/lab";
 import {useAppDispatch} from "../../app/store/configureStore";
@@ -19,6 +19,7 @@ const theme = createTheme();
 
 export default function Login() {
     const history = useHistory();
+    const location = useLocation<any>();
     const dispatch = useAppDispatch();
     const {register, handleSubmit, formState: {isSubmitting, errors, isValid}} = useForm({
         mode: 'all'
@@ -26,7 +27,7 @@ export default function Login() {
     async function submitForm(data: FieldValues) {
         try {
             await dispatch(sighInUser(data));
-            history.push('/catalog');
+            history.push(location.state?.from?.pathname || '/catalog');
         }
         catch (error) {
             console.log(error);
