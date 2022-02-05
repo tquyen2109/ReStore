355095391a3b91363d888c38d3a7d7eeb6602ef8
tquyen2109 <t.quyen1997@yahoo.com>
2022-02-04 21:49:44 -0500

    Order DTO and mock checkout form

---
+++ API/Entities/OrderAggregate/OrderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using API.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Entities.OrderAggregate
+{
+    public static class OrderExtensions
+    {
+        public static IQueryable<OrderDto> ProjectOrderToOrderDto(this IQueryable<Order> query)
+        {
+            return query
+                .Select(order => new OrderDto
+                {
+                    Id = order.Id,
+                    BuyerId = order.BuyerId,
+                    OrderDate = order.OrderDate,
+                    ShippingAddress = order.ShippingAddress,
+                    DeliveryFee = order.DeliveryFee,
+                    Subtotal = order.Subtotal,
+                    OrderStatus = order.OrderStatus.ToString(),
+                    Total = order.GetTotal(),
+                    OrderItem = order.OrderItem.Select(item => new OrderItemDto
+                    {
+                        ProductId = item.ItemOrdered.ProductId,
+                        Name = item.ItemOrdered.Name,
+                        PictureUrl = item.ItemOrdered.PictureUrl,
+                        Price = item.Price,
+                        Quantity = item.Quantity
+                    }).ToList()
+                }).AsNoTracking();
+        }
+    }
+}
